a25af6c2b1b1a1b728a6218d610df9dc8b49b00b
Fyzoriel | leo <leoseguin8+github@gmail.com>
2024-12-12 00:54:15 +0100

    fix: get menu without chat enabled

--- Twich UI Refresher/refresher.js
+++ Twich UI Refresher/refresher.js
@@ -1,17 +1,33 @@
 console.info("Fyzoriel's Twitch UI Refresher is running");
 
-const MENU_CLASS = "ScTransitionBase-sc-hx4quq-0 dOwqqa channel-root__upper-watch channel-root__upper-watch--with-chat tw-transition";
+const MENU_CLASS_CHAT = "ScTransitionBase-sc-hx4quq-0 dOwqqa channel-root__upper-watch channel-root__upper-watch--with-chat tw-transition";
+const MENU_CLASS = "ScTransitionBase-sc-hx4quq-0 dOwqqa channel-root__upper-watch channel-root__upper-watch tw-transition";
 const PARENT_CLASS = "channel-info-content";
 
-const observer = new MutationObserver(function (mutations, mutationInstance) {
+const getMenu = () => {
     const menu = document.getElementsByClassName(MENU_CLASS)[0];
+
+    if (menu) {
+        return menu;
+    }
+
+    return document.getElementsByClassName(MENU_CLASS_CHAT)[0];
+}
+
+const updateMenu = (menu, parent) => {
+    menu.style.width = "100%";
+    parent.prepend(menu);
+
+    console.info("Twitch stream menu updated successfully");
+}
+
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+
+    const menu = getMenu();
     const parent = document.getElementsByClassName(PARENT_CLASS)[0];
 
     if (menu && parent) {
-        menu.style.width = "100%";
-        parent.prepend(menu);
-
-        console.info("Twitch stream menu updated successfully");
+        updateMenu(menu, parent);
         mutationInstance.disconnect();
     }
 });
